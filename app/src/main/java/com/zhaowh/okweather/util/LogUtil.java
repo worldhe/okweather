de422141a37d89fac769e470b2e74a3cceb2c7f8
World <world_hehe@163.com>
2019-02-21 09:56:10 +0800

    修改日志系统的普通方法为静态方法static 	modified:   app/src/main/java/com/zhaowh/okweather/util/LogUtil.java

--- app/src/main/java/com/zhaowh/okweather/util/LogUtil.java
+++ app/src/main/java/com/zhaowh/okweather/util/LogUtil.java
@@ -13,31 +13,31 @@ public class LogUtil {
 
     public static int level = VERBOSE;
 
-    public void v(String tag, String msg){
+    public static void v(String tag, String msg){
         if(level <= VERBOSE){
             Log.v(tag, msg);
         }
     }
 
-    public void d(String tag, String msg){
+    public static void d(String tag, String msg){
         if(level <= DEBUG){
             Log.d(tag, msg);
         }
     }
 
-    public void i(String tag, String msg){
+    public static void i(String tag, String msg){
         if(level <= INFO){
             Log.i(tag, msg);
         }
     }
 
-    public void w(String tag, String msg){
+    public static void w(String tag, String msg){
         if(level <= WARN){
             Log.w(tag, msg);
         }
     }
 
-    public void e(String tag, String msg){
+    public static void e(String tag, String msg){
         if(level <= ERROR){
             Log.e(tag, msg);
         }
